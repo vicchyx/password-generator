b1fdce65e8c9dcdb6d57428c41b11b8ad0ad41fb
Victoria Castro <92071913+vicchyx@users.noreply.github.com>
2022-05-12 22:20:46 +1000

    Adding function/const for the code

--- assets/script.js
+++ assets/script.js
@@ -24,5 +24,12 @@ function writePassword() {
 
 }
 
+// Const properties - Function to retrieve the data once the user has selected their criteria
+function generatePassword() {
+	const Cletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+	const Lletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	const Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+	const SpcChar = ["?", "(" , "+", ".", "-", "!", "@", "#", "$", "%", "^", "&", "*"];
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
