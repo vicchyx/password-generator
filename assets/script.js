9040df888bc4cd34e5760dbf8fc88aece61cbacb
Victoria Castro <92071913+vicchyx@users.noreply.github.com>
2022-05-12 22:25:25 +1000

    Prompt to choose password length

--- assets/script.js
+++ assets/script.js
@@ -24,6 +24,14 @@ function writePassword() {
 
 }
 
+// Prompt to decide the length of password to be generated
+do {
+    passLen = prompt("Choose a password length (From 5-130 characters)");
+} while (passLen < 5 || passLen > 130);
+
+
+
+
 // Const properties - Function to retrieve the data once the user has selected their criteria
 function generatePassword() {
 	const Cletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
