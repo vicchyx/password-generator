03415459f3aa9c005c0463bcf2c325076ed5678a
Victoria Castro <92071913+vicchyx@users.noreply.github.com>
2022-05-12 22:29:03 +1000

    Adding prompt with questions

--- assets/script.js
+++ assets/script.js
@@ -24,13 +24,6 @@ function writePassword() {
 
 }
 
-// Prompt to decide the length of password to be generated
-do {
-    passLen = prompt("Choose a password length (From 5-130 characters)");
-} while (passLen < 5 || passLen > 130);
-
-
-
 
 // Const properties - Function to retrieve the data once the user has selected their criteria
 function generatePassword() {
@@ -38,6 +31,20 @@ function generatePassword() {
 	const Lletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 	const Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 	const SpcChar = ["?", "(" , "+", ".", "-", "!", "@", "#", "$", "%", "^", "&", "*"];
+
+    // Prompt to decide the length of password to be generated
+do {
+    passLen = prompt("Choose a password length (From 5-130 characters)");
+} while (passLen < 5 || passLen > 130);
+
+// Prompt with questions to user to select their criteria
+do {
+    Cletterscfm = confirm("Should we include uppercase letters?");
+    Lletterscfm = confirm("Should we include lowercase letters?");
+    Numbrscfm = confirm("What about if we include numbers?");
+    SpcCharcfm = confirm("Feeling like any special characters today?");
+} while (Cletterscfm === false && Lletterscfm === false && Numbrscfm === false && SpcCharcfm === false);
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
