2cd23297f266430d2bc5a5b6bc4047e51f557616
Victoria Castro <92071913+vicchyx@users.noreply.github.com>
2022-05-12 21:19:51 +1000

    Adding JS base code

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Variables list used for the password generator code
+var generateBtn = document.querySelector("#generate");
+var SpcCharcfm;
+var Numbrscfm;
+var Lletterscfm;
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
