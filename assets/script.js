aef28ad07e428d7c6dc1ac32f4e9dca6ec3dc327
Victoria Castro <92071913+vicchyx@users.noreply.github.com>
2022-05-12 22:17:25 +1000

    Adding variables

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,19 @@
 // Assignment Code
+
+//Code variables
 var generateBtn = document.querySelector("#generate");
+var SpcCharcfm;
+var Numbrscfm;
+var Lletterscfm;
+var Cletterscfm;
+var Cletters;
+var Lletters;
+var Numbers;
+var SpcChar;
+var pwd = [];
+var passLen;
+var password;
+var passwordText = "";
 
 // Write password to the #password input
 function writePassword() {
